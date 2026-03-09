c74c591ddfb8fc81d716625ab9cccbe7db5cd5b3
abc <abc@abc.abc>
2026-03-09 16:28:14 +0100

    maj

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+export default function Header() {
+    return (
+        <div>
+            <h1>CARTE</h1>
+            <div>
+                <button>Liste des marqueurs</button>
+                <button>Ajouter un marqueur</button>
+                <button>Modifier un marqueur</button>
+                <button>Supprimer un marqueur</button>
+            </div>
+        </div>
+    )
+}
